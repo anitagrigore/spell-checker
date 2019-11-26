622ee981e1133558d9bbf4aa07c575f635bb0d3a
Anita Grigore <anita.grigore1@gmail.com>
2019-11-26 14:44:20 +0200

    [WIP] Implement tests fot insert and remove

--- tests/trie_test.cpp
+++ tests/trie_test.cpp
@@ -4,11 +4,11 @@
 TEST(Trie, Construct)
 {
   Trie empty_trie;
-  
+
   ASSERT_TRUE(empty_trie.empty());
-  
+
   Trie initializer_list_trie{"foo", "bar"};
-  
+
   ASSERT_FALSE(initializer_list_trie.empty());
   ASSERT_EQ(initializer_list_trie.size(), 2);
 }
@@ -16,15 +16,27 @@ TEST(Trie, Construct)
 TEST(Trie, HasWord)
 {
   Trie trie{"foo", "bar"};
-  
+
   EXPECT_TRUE(trie.has_word("foo"));
   EXPECT_TRUE(trie.has_word("bar"));
   EXPECT_FALSE(trie.has_word("baz"));
 }
 
 TEST(Trie, Insert)
-{}
+{
+  Trie trie;
+
+  trie.insert("foo");
+  trie.insert("bar");
+  EXPECT_TRUE(trie.has_word("foo"));
+  EXPECT_TRUE(trie.has_word("bar"));  
+}
 
 TEST(Trie, Remove)
-{}
+{
+  Trie trie{"foo", "bar"};
 
+  EXPECT_TRUE(trie.remove("foo"));
+  EXPECT_TRUE(trie.remove("bar"));
+  EXPECT_FALSE(trie.remove("baz"));
+}
